5bf5860685f39c6da103766318784176189bb9b3
Christof Kloninger <kloningerchristof@gmail.com>
2026-01-30 17:47:52 +0100

    UIManager
    ADD Script UIManager
    UPD CanvasLyout into UIManager

--- Assets/Code/Managers/UIManager.cs
+++ Assets/Code/Managers/UIManager.cs
@@ -1,21 +1,63 @@
-using System;
-using Unity.VisualScripting;
+using System.Collections.Generic;
 using UnityEngine;
-
+using System;
+/// <remarks>
+///     <para>
+///         Author: Christof Kloninger <a href = "mailto: gme.24.kloninger@gmail.com>
+///     </para>
+///     <para>
+///         This is our glorified lightswitch operator. It listens to events and toggles the active panels
+///     </para>
+/// </remarks>
+/// <summary>
+/// 
+/// </summary>
 public class UIManager : MonoBehaviour
 {
-    [SerializeField] private GameObject HUDPanel;
-    [SerializeField] private GameObject MenuPanel;
-    [SerializeField] private GameObject OptionsPanel;
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
+    private static UIManager _instance;
+    #region Unity Editor
+    [SerializeField] private List<PanelData> panels;
+  #endregion
 
-    // Update is called once per frame
-    void Update()
+  #region MonoBehaviour
+  void Awake()
+  {
+    if ( _instance != null && _instance != this )
     {
-        
+        if( _instance != this) Destroy(this);
+        _instance = this;
+        DontDestroyOnLoad(this);
     }
-}
+  }
+  public void TogglePanel(PanelType type)
+    {
+        foreach ( var p in panels )
+        {
+            if(p.Type == type)
+            {
+                bool isActive = p.PanelObject.activeSelf;
+                p.PanelObject.SetActive(!isActive);
+            }
+            // Will probably extend functions here upon loading.
+            // Cerate an Enumerator for the Loading screen or Make a separate function
+        }
+    }
+    #endregion
+    #region Serializables
+    public enum PanelType
+    {
+        None,
+        HUD,
+        Menu,
+        Options,
+        Dialogue,
+        Loading
+    }
+    [Serializable]
+    public struct PanelData
+    {
+        public PanelType Type;
+        public GameObject PanelObject;
+    }
+    #endregion
+    }
