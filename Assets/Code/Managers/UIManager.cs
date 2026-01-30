1e5cea0614ac9becedd84cff060bcfa5fcbabee2
Christof Kloninger <kloningerchristof@gmail.com>
2026-01-30 16:38:08 +0100

    UIManager
    ADD Canvas Layout

---
+++ Assets/Code/Managers/UIManager.cs
@@ -0,0 +1,21 @@
+using System;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class UIManager : MonoBehaviour
+{
+    [SerializeField] private GameObject HUDPanel;
+    [SerializeField] private GameObject MenuPanel;
+    [SerializeField] private GameObject OptionsPanel;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
