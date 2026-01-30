0125919c14c66985ef0cfb11377685f9d5d96bbf
Dark <Feurigesdunkel29@gmail.com>
2026-01-30 22:58:13 +0100

    Sound
    Soundmanager, weiter

--- Assets/Script/Playlist/SoundManager.cs
+++ Assets/Script/Playlist/SoundManager.cs
@@ -2,16 +2,24 @@ using UnityEngine;
 
 public enum Soundtype
 {
-    
+  Maske_wuetend,
+  Maske_traurig,
+  Maske_lachen,
+  Maske_gleichgueltig,
+  Hintergrund01,
+  Hintergrund02
 }
 
 
-[RequireComponent(typeof(Audiosource))]
+[RequireComponent(typeof(AudioSource))]
 public class SoundManager : MonoBehaviour
 {
+    [SerializeField] private AudioClip[] soundlist;
     private static SoundManager instance;
     private AudioSource audioSource;
-
+    private float _masterVolume;
+    private float _musicVolume;
+    private float _sfxVolume;
     private void Awake()
     {
         instance = this;
@@ -24,6 +32,11 @@ public class SoundManager : MonoBehaviour
 
     public static void PlaySound(Soundtype sound, float volume = 1 )
     {
-
+        instance.audioSource.PlayOneShot(instance.soundlist[(int)sound], volume);
     }
+
+    public void SetMasterVolume(float volume) => _masterVolume = volume;
+    
+    public void SetMusicVolume(float volume) => _musicVolume = volume;
+    public void SetSoundVolume(float volume) => _sfxVolume = volume;
 }
